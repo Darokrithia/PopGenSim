c8a2574fb8f37c3ad6b945f0295c9fed7c8fe719
Darokrithia <dieltabin@gmail.com>
2018-08-14 15:10:08 -0600

    Added file for qepo

---
+++ qepo.c
@@ -0,0 +1,10 @@
+/**
+@file qepo.c
+@page qepo
+@author Daniel R. Tabin
+@brief Quantitive Evolition Polygenic Organism
+
+This program will be used to simulated Polygenic evoltion of
+quantitative traits by using QEPOs as defined above.
+*/
+
