b76928062693f5c0610c72aeeb17dba6c2b2a684
Darokrithia <dieltabin@gmail.com>
2018-08-15 12:31:13 -0600

    Split Qepo into header and base files

--- qepo.c
+++ qepo.c
@@ -7,24 +7,11 @@
 This program will be used to simulated Polygenic evoltion of
 quantitative traits by using QEPOs as defined above.
 */
-
-
-//Qepodia Rogerus
-struct Qepo {
-	double* dna_array;
-	double val;
-
-};
-
-Qepo* Qepo_new();
-void Qepo_mate(Qepo* location, Qepo* p1, Qepo p2);
-void Qepo_free(Qepo* q);
-
-int size;
+#include "qepo.h"
 
 Qepo* Qepo_new(){
 	Qepo* q = malloc(sizeof(Qepo));
-	q->dna_array = malloc(size*sizeof(double));
+	q->dna_array = malloc(chrom_size*sizeof(double));
 
 	return q;
 }
@@ -33,13 +20,13 @@ void Qepo_mate(Qepo* child, Qepo* p1, Qepo p2){
 	
 	//Cross over
 	//NOTE: this crossover algorith is very simple and will be suject to change
-	int split = (size/2);		//half and half
+	int split = (chrom_size/2);		//half and half
 	memcpy(child->dna_array, p1->dna_array, (split*sizeof(double)));	//get from p1
-	memcpy(child->dna_array, (p2->dna_array + (split*sizeof(double))), (size-split*sizeof(double)));	/get from p2
+	memcpy(child->dna_array, (p2->dna_array + (split*sizeof(double))), (chrom_size-split*sizeof(double)));	/get from p2
 
 	child-> val = 0;
 
-	for (int i = 0; i < size; i++){
+	for (int i = 0; i < chrom_size; i++){
 		//mutate
 		child->dna_array[i] += 0.0;
 		//calculate value
@@ -52,9 +39,4 @@ void Qepo_mate(Qepo* child, Qepo* p1, Qepo p2){
 void Qepo_free(Qepo* q){
 	free(q->dna_array);
 	free(q);
-}
-
-
-int main(int argc, char **argv) {
-	printf("yeet\n");
 }
---
+++ qepo.h
@@ -0,0 +1,13 @@
+//Qepodia Rogerus
+typedef struct Qepo Qepo;
+struct Qepo {
+	double* dna_array;
+	double val;
+
+};
+
+Qepo* Qepo_new();
+void Qepo_mate(Qepo* location, Qepo* p1, Qepo p2);
+void Qepo_free(Qepo* q);
+
+int chrom_size;
