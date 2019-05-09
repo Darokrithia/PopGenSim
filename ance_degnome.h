2834f774a2a05a4c86679c41d025fda4aa18a58d
Darokrithia <dieltabin@gmail.com>
2019-05-08 23:40:30 -0400

    Added ance_degnome

---
+++ ance_degnome.h
@@ -0,0 +1,23 @@
+#ifndef DEGNOME
+#define DEGNOME
+
+#include <stdlib.h>
+#include <gsl/gsl_rng.h>
+#include <gsl/gsl_randist.h>
+
+//Degnomedia Rogerus
+typedef struct Degnome Degnome;
+struct Degnome {
+	double* dna_array;
+	double hat_size;
+	int* GOI_array;		// goi = Gene Origin ID
+
+};
+
+Degnome* Degnome_new(void);
+void Degnome_mate(Degnome* location, Degnome* p1, Degnome* p2, gsl_rng* rng);
+void Degnome_free(Degnome* q);
+
+int chrom_size;
+
+#endif
