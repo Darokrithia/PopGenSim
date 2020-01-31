f479e7b13f5675b6e1a3c21d057eeec7ac229189
masalemi <masalemi29@gmail.com>
2020-01-31 16:42:49 -0500

    Added -h flag and help menu for polygensim

--- polygensim.c
+++ polygensim.c
@@ -17,24 +17,36 @@ struct JobData {
 };
 
 void usage(void);
+void help_menu(void);
 int jobfunc(void* p, void* tdat);
-
 double get_fitness(double hat_size);
 
-const char *usageMsg =
+const char* usageMsg =
 	"Usage: polygensim [-c chromosome_length] [-e mutation_effect]\n"
-	"\t\t  [-g num_generation] [-h help] [-m mutation_rate] \n"
+	"\t\t  [-g num_generations] [-h help] [-m mutation_rate] \n"
 	"\t\t  [-o crossover_rate] [-p population_size]\n";
 
-	// "Usage: polygensim [-c CL] [-p PS] [-g G] [-m MR] [-e ME] [-o CR]\n"
-	// "\n"
-	// "\"CL\" is chromosome length.  \"PS\" is the population size, and\n"
-	// "\"G\" is thenumber of generations that the simulation will\n"
-	// "run.  \"MR\" is mutation rate, \"ME\" is how much a mutation\n"
-	// "will effect a gene on average, and \"CR\" is crossover rate.\n"
-	// "They can be in any order, and not all are needed.\n"
-	// "Default CL is 50, default PS is 100, and the default G is\n"
-	// "1000.  Default MR is 1, default ME is 2 and defualt CR is 2\n";
+const char* helpMsg =
+	"OPTIONS\n"
+	"\t -c chromosome_length\n"
+	"\t\t Set chromosome length for the current simulation.\n"
+	"\t\t Default chromosome length is 50.\n\n"
+	"\t -e mutation_effect\n"
+	"\t\t Set how much a mutation will effect a gene on average.\n"
+	"\t\t Default mutation effect is 2.\n\n"
+	"\t -g num_generations\n"
+	"\t\t Set how many generations this simulation will run for.\n"
+	"\t\t Default number of generations is 1000\n\n"
+	"\t -h\t Display this help menu.\n\n"
+	"\t -m mutation_rate\n"
+	"\t\t Set the mutation rate for the current simulation.\n"
+	"\t\t Default mutation rate is 1.\n\n"
+	"\t -o crossover_rate\n"
+	"\t\t Set the crossover rate for the current simulation.\n"
+	"\t\t Default crossover rate is 2.\n\n"
+	"\t -p population_size\n"
+	"\t\t Set the population size for the current simulation.\n"
+	"\t\t Default population size is 100.\n\n";
 
 pthread_mutex_t seedLock = PTHREAD_MUTEX_INITIALIZER;
 unsigned long rngseed = 0;
@@ -82,6 +94,11 @@ void usage(void) {
 	exit(EXIT_FAILURE);
 }
 
+void help_menu(void) {
+	fputs(helpMsg, stderr);
+	exit(EXIT_FAILURE);
+}
+
 double get_fitness(double hat_size){
 	return hat_size;
 }
@@ -95,7 +112,7 @@ int main(int argc, char **argv){
 	mutation_effect = 2;
 	crossover_rate = 2;
 
-	if(argc > 13 || (argc%2) == 0){
+	if(argc > 13){
 		// printf("\n");
 		usage();
 	}
@@ -118,6 +135,9 @@ int main(int argc, char **argv){
 			}
 			else if (strcmp(argv[i], "-o") == 0){
 				sscanf(argv[i+1], "%u", &crossover_rate);
+			}
+			else if (strcmp(argv[i], "-h") == 0){
+				help_menu();
 			}
 			else{
 				printf("\n");
