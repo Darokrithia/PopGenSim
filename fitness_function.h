182cf80cc5a01c2d68cf59c3a9c18fa8f5310f53
Darokrithia <dieltabin@gmail.com>
2020-02-04 16:57:45 -0500

    .

--- fitness_function.h
+++ fitness_function.h
@@ -1,11 +1,12 @@
 #ifndef FITFUNC
 #define FITFUNC
 
-typedef double* (*func)(double*) fit_func_ptr;		//pointer to a fitness function which takes and returns a double
+typedef double* (*fit_func_ptr)(double);		//pointer to a fitness function which takes and returns a double
 
-char func_name[8];									//used for command line args
+//char func_name[8];							//used for command line args (may not be needed on second thought delete later)
 double input;
 fit_func_ptr func_to_run;
 
+void set_function(char*);
 
 #endif
