324382232644db372c830f3082d506dc06c746d9
Darokrithia <dieltabin@gmail.com>
2020-02-14 16:46:31 -0500

    fleshed out the new functions

--- fitness_function.c
+++ fitness_function.c
@@ -15,6 +15,41 @@
 
 #import "fitness_function.h"
 
-void set_function(char*){
-	func_to_run = &linear_returns;
+void set_function(char* func_name) {
+	if(strcmp(func_name, "linear")) {
+		func_to_run = &linear_returns;
+	}
+	else if (strcmp(func_name, "sqrt")){
+		func_to_run = &sqrt_returns;
+	}
+	else if (strcmp(func_name, "close")){
+		func_to_run = &close_returns;
+	}
+	else if (strcmp(func_name, "ceiling")){
+		func_to_run = &ceiling_returns;
+	}
+	else {
+		func_to_run = &linear_returns;
+	}
+}
+
+double linear_returns(double input) {
+	return input;
+}
+
+double sqrt_returns(double input) {
+	return sqrt(input);
+}
+
+double close_returns(double input) {
+	return (target_num - abs(target_num - input));
+}
+
+double ceiling_returns(double input) {
+	if (input < target_num) {
+		return input;
+	}
+	else {
+		(target_num) - 10 * abs(target_num - input);
+	}
 }
--- fitness_function.h
+++ fitness_function.h
@@ -5,6 +5,7 @@ typedef double* (*fit_func_ptr)(double);		//pointer to a fitness function which 
 
 //char func_name[8];							//used for command line args (may not be needed on second thought delete later)
 double input;
+double target_num;
 fit_func_ptr func_to_run;
 //(*func_to_run)(hat_height)
 
