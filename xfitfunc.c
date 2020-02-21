7be6a6b977ae7cabc228030ed4978adb6621c392
Darokrithia <dieltabin@gmail.com>
2020-02-21 17:00:58 -0500

    Fixed bugs and started off xfitfunc

--- xfitfunc.c
+++ xfitfunc.c
@@ -9,7 +9,20 @@
 #include <stdlib.h>
 #include <string.h>
 
-int main(int argc, char **argv){
-	
+int main(int argc, char **argv) {
+	double x = 100;
+
+	if(argc == 2) {
+		if(strncmp(argv[1], "-v", 2) != 0) {
+			fprintf(stderr, "usage: xfitfunc [-v]\n");
+			exit(EXIT_FAILURE);
+		}
+		verbose = 1;
+	}
+	else if(argc != 1) {
+		fprintf(stderr, "usage: xfitfunc [-v]\n");
+		exit(EXIT_FAILURE);
+	}
+
 	printf("All tests for xfitfunc completed\n");
 }
