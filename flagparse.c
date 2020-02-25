e709300839388d186d420d4ecc33ed0c6334b03b
masalemi <masalemi29@gmail.com>
2020-02-25 16:35:59 -0500

    Updated parse flags arguments

--- flagparse.c
+++ flagparse.c
@@ -1,6 +1,11 @@
 #include "flagparse.h"
 #include <string.h>
 
-int parse_flags(int * flags) {
+int parse_flags(int argc, char ** argv, int * flags) {
+
+
+	// Flags formatting:
+	// flags[0] ->
+
 	return 0;
 }
--- flagparse.h
+++ flagparse.h
@@ -1,6 +1,6 @@
 #ifndef FLAGPARSE
 #define FLAGPARSE
 
-int parse_flags(int * flags);
+int parse_flags(int argc, char ** argv, int * flags);
 
 #endif
