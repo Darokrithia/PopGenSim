3c4aa037223253579d2261b4ca12527b7d6d88a1
Katherine Roughan <46721827+roughk@users.noreply.github.com>
2020-04-17 16:49:49 -0400

    new universal degnome
    universal degnome, some changes need to be made to other programs to make it truly universal.

---
+++ new_degnome.h
@@ -0,0 +1,24 @@
+#ifndef DEGNOME
+#define DEGNOME
+
+#include <stdlib.h>
+#include <gsl/gsl_rng.h>
+#include <gsl/gsl_randist.h>
+
+//Degnomedia Rogerus
+typedef struct Degnome Degnome;
+struct Degnome {
+	double* dna_array;
+	double hat_size;
+	int* GOI_array;		// goi = Gene Origin ID
+
+};
+
+Degnome* Degnome_new(void);
+void Degnome_mate(Degnome* location, Degnome* p1, Degnome* p2, gsl_rng* rng,
+	int mutation_rate, int mutation_effect, int crossover_rate);
+void Degnome_free(Degnome* q);
+
+int chrom_size;
+
+#endif
